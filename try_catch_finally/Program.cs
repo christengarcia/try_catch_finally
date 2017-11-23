7f6f791b1ac1ec1ad77140ff5c4301ddeb0f88b4
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-23 22:19:55 +1100

    First commit

---
+++ try_catch_finally/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace try_catch_finally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerator, denominator;
+
+            Console.Write("Please enter the numerator: ");
+            numerator = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter the denominator: ");
+            denominator = Convert.ToInt32(Console.ReadLine());
+
+            // try-catch-finally statement controls how the 
+            // program proceeds when an error occurs. 
+            try{
+                Console.WriteLine("The result is {0}.", numerator/denominator);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("---- End of Error Handling Example ----");
+            }
+        }
+    }
+}
